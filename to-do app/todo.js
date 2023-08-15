de684a1919cbf04922fd809f271fb9a0ed7d95fc
Arvind <arvindcool2022@gmail.com>
2023-08-15 06:35:56 +0530

    todo-list completed (added drag & drop)

--- to-do app/todo.js
+++ to-do app/todo.js
@@ -1,37 +1,51 @@
 const input = document.getElementById('input');
-const addButton = document.querySelector('button');
+const addButton = document.querySelector('.add');
+const clearAllButton = document.querySelector('.clear');
 const todoList = document.querySelector('.list');
 const errorMessage = document.querySelector('#err');
-
+let dragged;
 loadItemsFromStorage();
 
 addButton.addEventListener('click', addTask);
+clearAllButton.addEventListener('click', deleteAllTask);
 todoList.addEventListener('click', handleListClick);
+todoList.addEventListener('dragstart', handleDragStart);
+todoList.addEventListener('dragend', handleDragEnd);
+todoList.addEventListener('dragover', handleDragOver);
+todoList.addEventListener('drop', handleDrop);
 
 function addTask() {
   if (input.value.trim() !== '') {
     const li = document.createElement('li');
     li.innerHTML = input.value.trim();
+    li.setAttribute('draggable', 'true');
     todoList.appendChild(li);
     const deleteButton = document.createElement('span');
     deleteButton.innerHTML = '\u00d7'; //multiply symbol 'x'
     li.appendChild(deleteButton);
     input.value = '';
-    saveItemsFromStorage();
+    saveItemsToStorage();
     errorMessage.style.display = 'none';
   } else {
     errorMessage.setAttribute('style', 'display: block;');
   }
 }
 
+function deleteAllTask() {
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.firstChild);
+  }
+  saveItemsToStorage();
+}
+
 function handleListClick(event) {
   const clickedElement = event.target;
   if (clickedElement.tagName === 'LI') {
     clickedElement.classList.toggle('checked');
-    saveItemsFromStorage();
+    saveItemsToStorage();
   } else if (clickedElement.tagName === 'SPAN') {
     clickedElement.parentElement.remove();
-    saveItemsFromStorage();
+    saveItemsToStorage();
   }
 }
 
@@ -40,6 +54,45 @@ function loadItemsFromStorage() {
   if (items) todoList.innerHTML = items;
 }
 
-function saveItemsFromStorage() {
+function saveItemsToStorage() {
   localStorage.setItem('todoItems', todoList.innerHTML);
+}
+
+function handleDragStart(event) {
+  event.dataTransfer.setData('text/plain', event.target.innerHTML);
+  // event.target.remove();
+  dragged = event.target;
+  console.log(dragged);
+
+  dragged.setAttribute('class', 'dragging');
+}
+
+function handleDragEnd() {
+  dragged.classList.remove('dragging');
+}
+
+function handleDragOver(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+}
+
+function handleDrop(event) {
+  event.preventDefault();
+  const draggedText = event.dataTransfer.getData('text/plain');
+  const li = document.createElement('li');
+  li.innerHTML = draggedText;
+  li.setAttribute('draggable', 'true');
+
+  // Determine the drop target position
+  const dropTarget = event.target.closest('li');
+  if (dropTarget === todoList.firstChild) {
+    todoList.insertBefore(li, todoList.firstChild);
+  } else if (dropTarget) {
+    todoList.insertBefore(li, dropTarget.nextSibling);
+  } else {
+    todoList.appendChild(li);
+  }
+  dragged.remove();
+
+  saveItemsToStorage();
 }
